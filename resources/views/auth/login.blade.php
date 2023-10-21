3beb5bd58098254ed58de26bdad6597652bb06ed
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2023-10-22 04:08:56 +0700

    fix auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,74 @@
-<!--
-Author: Colorlib
-Author URL: https://colorlib.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 
 <head>
-    <title>{{ env('APP_COMPANY') }}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
     <link rel="icon" href="{{ asset(env('APP_COMPANY_LOGO')) }}" type="image/x-icon" />
-    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-    <!-- Custom Theme files -->
-    <link href="{{ asset('assets/customer/css/auth.css') }}" rel="stylesheet" type="text/css" media="all" />
-    <!-- //Custom Theme files -->
-    <!-- web font -->
-    <link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
-    <!-- //web font -->
+
+    <link rel="stylesheet" href="/assets/customer/auth/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="/assets/customer/auth/css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/assets/customer/auth/css/bootstrap.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="/assets/customer/auth/css/style.css">
+
+    <title>{{ env('APP_COMPANY') }}</title>
 </head>
 
 <body>
-    <!-- main -->
-    <div class="main-w3layouts wrapper">
-        <h1>{{ env('APP_COMPANY') }} Login Form</h1>
-        <div class="main-agileinfo">
-            <div class="agileits-top">
-                @if (session()->has('errors'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ session('errors') }}
+    <div class="content">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 contents">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12">
+                            <div class="form-block">
+                                <div class="mb-4">
+                                    <h3><strong>{{ env('APP_COMPANY') }}</strong> Login </h3>
+                                    @if (session()->has('errors'))
+                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                            {{ session('errors') }}
+                                        </div>
+                                    @endif
+                                </div>
+                                <form action="{{ route('auth.login.process') }}" method="post">
+                                    @csrf
+                                    <div class="form-group first">
+                                        <label for="email">email</label>
+                                        <input type="email" class="form-control" id="email" name="email"
+                                            value="{{ old('email') }}" autofocus required>
+                                    </div>
+                                    <div class="form-group last mb-4">
+                                        <label for="password">password</label>
+                                        <input type="password" class="form-control" id="password" name="password"
+                                            required>
+                                    </div>
+
+
+                                    <input type="submit" value="Login"
+                                        class="btn btn-pill text-white btn-block btn-primary mb-2">
+                                    <span>Tidak Mempunyai akun? <a href="/auth/register"
+                                            class="text-primary">register</a></span>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                @endif
-                <form action="{{ route('auth.login.process') }}" method="post">
-                    @csrf
-                    <input class="text email" type="email" name="email" placeholder="Email" required="">
-                    <input class="text" type="password" name="password" placeholder="Password" required="">
-                    <input type="submit" value="Login">
-                </form>
-                <p>Belum Sudah Memiliki Akun? <a href="{{ route('auth.register') }}"> Register Sekarang!</a></p>
+
+                </div>
+
             </div>
         </div>
-        <ul class="colorlib-bubbles">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
     </div>
-    <!-- //main -->
+    <script src="/assets/customer/auth/js/jquery-3.3.1.min.js"></script>
+    <script src="/assets/customer/auth/js/popper.min.js"></script>
+    <script src="/assets/customer/auth/js/bootstrap.min.js"></script>
+    <script src="/assets/customer/auth/js/main.js"></script>
 </body>
 
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,105 +6,98 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+    <link rel="icon" href="{{ asset(env('APP_COMPANY_LOGO')) }}" type="image/x-icon" />
 
-    <link rel="stylesheet" href="{{ asset('assets/customer/auth') }}/fonts/icomoon/style.css">
+    <link rel="stylesheet" href="/assets/customer/auth/fonts/icomoon/style.css">
 
-    <link rel="stylesheet" href="{{ asset('assets/customer/auth') }}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/assets/customer/auth/css/owl.carousel.min.css">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/customer/auth') }}/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/customer/auth/bootstrap.min.css">
 
-    <!-- Style -->
-    <link rel="stylesheet" href="{{ asset('assets/customer/auth') }}/css/style.css">
 
-    <title>Login #7</title>
+    <title>{{ env('APP_COMPANY') }}</title>
 </head>
 
 <body>
     <div class="content">
         <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="{{ asset('assets/customer/auth') }}/images/undraw_remotely_2j6y.svg" alt="Image"
-                        class="img-fluid">
-                </div>
-                <div class="col-md-6 contents">
+            <div class="row justify-content-center">
+                <div class="col-md-10 contents  bg-white py-5 px-5 mt-5 rounded">
                     <div class="row justify-content-center">
-                        <div class="col-md-8">
-                            <div class="mb-4">
-                                <h3>{{ env('APP_COMPANY') }} Register</h3>
-                                {{-- <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur
-                                    adipisicing.</p> --}}
-                            </div>
-                            <form action="{{ route('auth.register.process') }}" method="post">
-                                @csrf
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input type="text" name="name" value="{{ old('name') }}"
-                                        class="form-control" id="name">
+                        <div class="col-md-12">
+                            <div class="form-block">
+                                <div class="mb-4">
+                                    <h3><strong>{{ env('APP_COMPANY') }}</strong> Register </h3>
+                                    @if (session()->has('warning'))
+                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                            {{ session('warning') }}
+                                        </div>
+                                    @endif
                                 </div>
-                                @error('name')
-                                    <span class="help-block" style="color:red;">{{ $message }}</span>
-                                    <br>
-                                @enderror
-                                <br>
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="email" name="email" value="{{ old('email') }}"
-                                        class="form-control" id="email">
-                                </div>
-                                @error('email')
-                                    <span class="help-block" style="color:red;">{{ $message }}</span>
-                                    <br>
-                                @enderror
-                                <br>
-                                <div class="form-group">
-                                    <label for="phone_number">No. hp</label>
-                                    <input type="text" name="phone_number" value="{{ old('phone_number') }}"
-                                        class="form-control" id="phone_number">
-                                </div>
-                                @error('phone_number')
-                                    <span class="help-block" style="color:red;">{{ $message }}</span>
-                                    <br>
-                                @enderror
-                                <br>
-                                <div class="form-group">
-                                    <label for="password">Password</label>
-                                    <input type="password" name="password" value="{{ old('password') }}"
-                                        class="form-control" id="password">
-                                </div>
-                                @error('password')
-                                    <span class="help-block" style="color:red;">{{ $message }}</span>
-                                    <br>
-                                @enderror
-                                <br>
-                                <div class="form-group">
-                                    <label for="password_confirmation">Konfirmasi Password</label>
-                                    <input type="password" name="password_confirmation"
-                                        value="{{ old('password_confirmation') }}" class="form-control"
-                                        id="password_confirmation">
-                                </div>
-                                @error('password_confirmation')
-                                    <span class="help-block" style="color:red;">{{ $message }}</span>
-                                @enderror
-                                <br>
+                                <form action="{{ route('auth.register.process') }}" method="post">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label class="form-label" for="name">Name</label>
+                                        <input type="text" class="form-control" value="{{ old('name') }}"
+                                            id="name" name="name" required>
+                                        @error('name')
+                                            <span class="help-block" style="color:red;">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label" for="email">Email</label>
+                                        <input type="email" class="form-control"value="{{ old('email') }}"
+                                            id="email" name="email" required>
+                                        @error('email')
+                                            <span class="help-block" style="color:red;">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label" for="phone_number">No. hp</label>
+                                        <input type="text" class="form-control" value="{{ old('phone_number') }}"
+                                            id="phone_number" name="phone_number" required>
+                                        @error('phone_number')
+                                            <span class="help-block" style="color:red;">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label" for="password">password</label>
+                                        <input type="password" class="form-control" value="{{ old('password') }}"
+                                            id="password" name="password" required>
+                                        @error('password')
+                                            <span class="help-block" style="color:red;">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label" for="password_confirmation">konfirmasi
+                                            password</label>
+                                        <input type="password" class="form-control"
+                                            value="{{ old('password_confirmation') }}" id="password_confirmation"
+                                            name="password_confirmation" required>
+                                        @error('password_confirmation')
+                                            <span class="help-block" style="color:red;">{{ $message }}</span>
+                                        @enderror
+                                    </div>
 
-                                <input type="submit" value="Register" class="btn btn-block btn-primary">
-                            </form>
+
+                                    <input type="submit" value="Register"
+                                        class="btn btn-pill text-white btn-block btn-primary mb-2">
+                                    <span>Mempunyai akun? <a href="/auth/login" class="text-primary">Login</a></span>
+                                </form>
+                            </div>
                         </div>
                     </div>
 
                 </div>
-
             </div>
         </div>
     </div>
-
-
-    <script src="{{ asset('assets/customer/auth') }}/js/jquery-3.3.1.min.js"></script>
-    <script src="{{ asset('assets/customer/auth') }}/js/popper.min.js"></script>
-    <script src="{{ asset('assets/customer/auth') }}/js/bootstrap.min.js"></script>
-    <script src="{{ asset('assets/customer/auth') }}/js/main.js"></script>
+    </div>
+    <script src="/assets/customer/auth/js/jquery-3.3.1.min.js"></script>
+    <script src="/assets/customer/auth/js/popper.min.js"></script>
+    <script src="/assets/customer/auth/js/bootstrap.min.js"></script>
+    <script src="/assets/customer/auth/js/main.js"></script>
 </body>
 
 </html>
